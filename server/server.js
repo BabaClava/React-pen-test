55ef526786cb88d16f334a451a2ad263199c2629
Super <super.babaclava@gmail.com>
2019-05-20 21:42:47 +0300

    res/query

--- server/server.js
+++ server/server.js
@@ -2,29 +2,48 @@ const http = require('http');
 const fs = require('fs');
 const url = require('url');
 
-http.createServer((req, res) => {
-console.log(url.parse(req.url, true).query);
-// count:10
-// page:1
-   
-    if (req.url === '/users') {
-        let data = JSON.parse(fs.readFileSync('./data-mocks/users-data.json', 'utf-8'));
-        // let a = [];
-        // for (let i = 2; i < 4; i++) {
-        //     a.push(data[i]);
-        // }
-        // console.log(a);
-        
-        
-        const headers = {
-            'Access-Control-Allow-Origin': '*',
-            'Access-Control-Allow-Methods': 'OPTIONS, POST, GET',
-            'Access-Control-Max-Age': 2592000, // 30 days
-            "Content-Type": "application/json"
-            /** add other headers as per requirement */
-          };
-        res.writeHead(200, headers)
-        res.end(JSON.stringify(data, null, '\t'));
+const getRes = (data, query) => {
+    let count = Number(query.count) || 10,
+        page = Number(query.page) || 1,
+        items = [];
+
+    if (count>100) count = 100;
+
+    if (page == 1) {
+        items = data.slice(0, count);
+    } else {
+        let start = (page - 1) * count;
+        let end = page * count;
+        items = data.slice(start, end);
     }
-    res.end('test\n');
-}).listen(8080, () => console.log('Server start'));
+    return result = {items, TotalCount: data.length};
+
+}
+
+const server = (req, res) => {
+    let query = url.parse(req.url, true).query;
+    console.log(query);
+    let path = url.parse(req.url).pathname;
+    console.log(path);
+    
+    // count:10
+    // page:1
+       
+        if (path === '/users') {
+            let data = JSON.parse(fs.readFileSync('./data-mocks/users-data.json', 'utf-8'));
+            let result = getRes(data, query);
+            
+            const headers = {
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Methods': 'OPTIONS, POST, GET',
+                'Access-Control-Max-Age': 2592000, // 30 days
+                "Content-Type": "application/json"
+                /** add other headers as per requirement */
+              };
+            res.writeHead(200, headers)
+            res.end(JSON.stringify(result, null, '\t'));
+        }
+        res.end('test\n');
+    };
+
+http.createServer(server).listen(8080, () => console.log('Server start'));
