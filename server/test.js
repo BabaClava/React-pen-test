eb3c25235170c3128a08f79fa60046ee4bcc4f72
Super <super.babaclava@gmail.com>
2019-06-02 23:12:30 +0300

    complete router

--- server/test.js
+++ server/test.js
@@ -8,15 +8,17 @@ const PORT = 3000;
 const routing = {
     '/api/users': 'users data, need query or default params',
     '/api/profile/status/:id': 'profile status, need $id. Coming soon',
-    '/api/profile/:id': 'profile data, need $id'        //like an express, but we can use any SPECIFIC character and replace his later;
+    '/api/profile/:id': 'profile data, need $id'       //like an express, but we can use ANY SPECIFIC character and replace his later;
 };
 
 const matching = [];        //puts all routes like a "/path/:id" in new array
 for (const key in routing) {
     if (key.includes(':')) {        // ':' - it`s a random mark that we used in routing before
+        const paramsKeys = [];
+        key.match(/:[\w]+/g).forEach( par => paramsKeys.push(par.slice(1)) );
         const rx = new RegExp( (key+'/?$').replace(/:[\w]+/g, '(\\d+)') );
         const handler = routing[key];
-        matching.push([rx, handler]);
+        matching.push([rx, handler, paramsKeys]);
         delete routing[key];
     }
     /*can add additional type of replaceable mark*/
@@ -26,32 +28,42 @@ const types = {
     'string': str => str,
     'number': num => num.toString,
     'object': JSON.stringify,
-    'function': (client, fn) => JSON.stringify(fn(client)),
+    'function': (fn, client, params) => JSON.stringify(fn(client, params)),
     'undefined': () => 'path not found'
 };
 
 const router = client => {
-    let params;
-    let handler = routing[client.req.url];
+    const parted_url = url.parse(client.req.url, true);
+    const params = {};
+    params.query = parted_url.query;
+    let handler = routing[parted_url.pathname];
     if (!handler) {
         for (let i = 0; i < matching.length; i++) {
             const rx = matching[i];
-            params = client.req.url.match(rx[0]);
-            if (params) {
-                params.shift();     //remove path and leave only specific parameters
+            let paramsValues = parted_url.pathname.match(rx[0]);
+            if (paramsValues) {
                 handler = rx[1];
+                paramsValues.shift();     //remove path and leave only specific parameters
+                let paramsKeys = rx[2]
+                for (let i = 0; i < paramsKeys.length; i++) {
+                    params[(paramsKeys[i])] = paramsValues[i];
+                }
+                break;
             }
         }
     }
     const type = typeof handler;
-    const serializer = types[type];
-    const result = serializer(handler, client);
-    client.res.end(result);
+    const serializer = types[type];    
+    const result = serializer(handler, client, params);
+    client.res.end(JSON.stringify(result)); //убрать в кб
 }
 
 http.createServer((req,res)=>{
     router({req, res});
+}).listen(PORT, ()=>console.log(`test start on port:${PORT}\r\n`))
+    .on("error", err => {
+        if (err.code === "EADDRINUSE") {
+            console.error(`No access to port: ${PORT}\r\n`);
+        }
+    });
 
-    res.writeHead(200, {"Content-Type": "text/plain"});
-    res.end('Not');
-}).listen(PORT, ()=>console.log(`test start on port:${PORT}`))
