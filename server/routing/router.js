48857d55331abbddde8d57db0ba42b8d269c9bcd
Super <super.babaclava@gmail.com>
2019-06-14 16:09:34 +0300

    fashion

--- server/routing/router.js
+++ server/routing/router.js
@@ -3,38 +3,38 @@
 const url = require('url');
 
 const RoutingCreator = require('../lib/RoutingCreator'),
-      Serializer = require('../lib/Serializer.js'),
-      usersHandler = require('./requestHandlers/users');
+	  Serializer = require('../lib/Serializer.js'),
+	  usersHandler = require('./requestHandlers/users');
 
 const [exact, matching] = RoutingCreator({
-        '/api/users': usersHandler,
-        '/api/profile/status/:id': 'profile status, need $id. Coming soon',
-        '/api/profile/:id': 'profile data, need $id'       //like an express, but we can use ANY SPECIFIC character and replace his later;
-    });
+		'/' : 'root',
+		'/api/users': usersHandler,
+		'/api/profile/status/:id': 'profile status, need $id. Coming soon',
+		'/api/profile/:id': 'profile data, need $id'       //like an express, but we can use ANY SPECIFIC character and replace his later;
+	});
 
 const router = client => {
-    const parted_url = url.parse(client.req.url, true);
-    const params = {};
-    params.query = parted_url.query;
-    let handler = exact[parted_url.pathname];
-    if (!handler) {
-        for (let i = 0; i < matching.length; i++) {
-            const rx = matching[i];
-            let paramsValues = parted_url.pathname.match(rx[0]);
-            if (paramsValues) {
-                handler = rx[1];
-                paramsValues.shift();     //remove path and leave only specific parameters
-                let paramsKeys = rx[2]
-                for (let i = 0; i < paramsKeys.length; i++) {
-                    params[(paramsKeys[i])] = paramsValues[i];
-                }
-                break;
-            }
-        } 
-    }
+	const parted_url = url.parse(client.req.url, true);
+	const params = {};
+	params.query = parted_url.query;
+	let handler = exact[parted_url.pathname];
+	if (!handler) {
+		for (let i = 0; i < matching.length; i++) {
+			const rx = matching[i];
+			let paramsValues = parted_url.pathname.match(rx[0]);
+			if (paramsValues) {
+				handler = rx[1];
+				paramsValues.shift();     //remove path and leave only specific parameters
+				let paramsKeys = rx[2]
+				for (let i = 0; i < paramsKeys.length; i++) {
+					params[(paramsKeys[i])] = paramsValues[i];
+				}
+				break;
+	    	}
+		}
+	}
 
-    Serializer(handler, client, params);
+	Serializer(handler, client, params);
 }
-
 
 module.exports = router;
--- server/server.js
+++ server/server.js
@@ -1,21 +1,19 @@
 'use strict'
 const http = require('http'),
-      router = require('./routing/router');
+	  router = require('./routing/router');
 
 const PORT = 3002;
 
-const server = http.createServer((req, res) => {
-    router({req, res});
-});
-    
-server.on("error", err => {
-    if (err.code === "EADDRINUSE") {
-    console.error(`No access to port: ${PORT}\r\n`);
-    }
-});
-
-server.on('clientError', (err, socket) => {
-    socket.end('HTTP/1.1 400 Bad Request\r\n');
-});
-
-server.listen(PORT, () => console.log(`Server start on port:${PORT}\r\n`));
+http
+	.createServer((req, res) => {
+    	router({ req, res });
+	})
+    .on("error", err => {
+        if (err.code === "EADDRINUSE") {
+            console.error(`No access to port: ${PORT}\r\n`);
+        }
+    })
+    .on("clientError", (err, socket) => {
+        socket.end("HTTP/1.1 400 Bad Request\r\n");
+    })
+    .listen(PORT, () => console.info(`Server start on port:${PORT}\r\n`));
