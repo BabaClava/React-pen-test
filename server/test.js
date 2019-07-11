3448c93e7292b124c7820d5cb1bfd0f98c9c3378
Super <super.babaclava@gmail.com>
2019-07-11 16:39:26 +0300

    reworked

--- server/test.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-
-const http = require('http')
-
-http.createServer((req, res) => {
-    console.log(process.argv);
-    res.end();
-}).listen(3002, ()=>console.log('server run on port: 3002'));
